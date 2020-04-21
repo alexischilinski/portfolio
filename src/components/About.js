ba572fae3ddb3e8c6927b0a3be1f3afa0fae4cd4
alexiscait142 <chilinski.a@gmail.com>
2020-04-21 12:48:35 -0400

    added node to known languages

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export const About = () => {
                 {/* <img className="about-photo" src={headshot}></img> */}
                 <div className="about-blurb">
                     <p>I am a software engineer from Boston, Massachusetts. I build full-stack applications in an array of languages and frameworks including
-                        Ruby on Rails, Python, Django, and React.
+                        Ruby on Rails, Node, Python, Django, and React.
                     </p>
                     <p>I come from an extensive background in customer service for industries such as airlines and theme parks. In my spare time,
                         you'll most likely find me out for a run, playing guitar, or watching Star Wars.
