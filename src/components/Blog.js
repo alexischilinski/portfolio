5c083ef19a2069960f6e4b8d8471fd2c861812ec
alexiscait142 <chilinski.a@gmail.com>
2020-08-04 13:55:14 -0400

    Fix overall styling and added more blogposts.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -15,12 +15,20 @@ export const Blog = () => {
                     <a href="https://medium.com/@chilinski.a/how-to-seed-a-django-api-with-data-from-an-external-api-b577b6e6ad54" target="_blank"><img className="blog-photo" src="https://images.unsplash.com/photo-1462690417829-5b41247f6b0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"></img></a>
                 </div>
                 <div className="blog-description">
-                    <h1>Deleting a record from a join-table without deleting records from other tables</h1>
+                    <h1>Deleting a record from a join-table without deleting records from other tables - Ruby on Rails</h1>
                     <a href="https://medium.com/@chilinski.a/deleting-a-record-from-a-join-table-without-deleting-records-from-other-tables-ruby-on-rails-1bf832c7a479" target="_blank"><img className="blog-photo" src="https://images.unsplash.com/photo-1526270100824-f033a2377d62?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjQ1NDExfQ&auto=format&fit=crop&w=3819&q=80"></img></a>
                 </div>
                 <div className="blog-description">
                     <h1>NPM scripts and concurrently — utilizing Node.js tools</h1>
                     <a href="https://medium.com/@chilinski.a/npm-scripts-and-concurrently-utilizing-node-js-tools-9e91b8c6c1dd" target="_blank"><img className="blog-photo" src="https://images.unsplash.com/photo-1544980919-e17526d4ed0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3451&q=80"></img></a>
+                </div>
+                <div className="blog-description">
+                    <h1>Python: Web-scraping to CSVs</h1>
+                    <a href="https://medium.com/analytics-vidhya/django-web-scraping-to-csvs-a1082d41ca64" target="_blank"><img className="blog-photo" src="https://www.corporatecomplianceinsights.com/wp-content/uploads/2018/06/spreadsheet.jpg"></img></a>
+                </div>
+                <div className="blog-description">
+                    <h1>Setting up a Basic Django API</h1>
+                    <a href="https://medium.com/swlh/setting-up-a-basic-django-backend-5f0b746717e8" target="_blank"><img className="blog-photo" src="https://i0.wp.com/coding-sessions.com/wp-content/uploads/2020/02/drf_4_api_data_1.png?ssl=1://www.django-rest-framework.org/img/quickstart.png"></img></a>
                 </div>
             </div>
         </>
