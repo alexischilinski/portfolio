583687c265d73091d3a4cae428fce4dd356e18e1
alexiscait142 <chilinski.a@gmail.com>
2020-08-04 15:36:03 -0400

    Add resume.

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import headshot from './photos/headshot.jpg'
+import Resume from './photos/AlexisResume.pdf'
 
 export const About = () => {
     return (
@@ -11,6 +12,8 @@ export const About = () => {
                 <div className="about-blurb">
                     <p>I am a software engineer from Boston, Massachusetts. I build full-stack applications in an array of languages and frameworks including
                         Ruby on Rails, Node, Python, Django, and React.
+                        I continue to enhance my engineering expertise by experimenting with different languages and technologies
+                        and by working on complex freelance projects.
                     </p>
                     <p>I come from an extensive background in customer service for industries such as airlines and theme parks. In my spare time,
                         you'll most likely find me out for a run, playing guitar, or watching Star Wars.
@@ -23,6 +26,7 @@ export const About = () => {
                         <a className="blog icon" href={"https://medium.com/@chilinski.a"} target="_blank"></a>
                     </div>
                 </ul>
+                <a className="resume" href={Resume} target="_blank">Click Here to View Resume</a>
             </div>
         </>
     )
