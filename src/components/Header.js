9c9b9e866c46e637531b26b87c9bde187dd638c6
alexiscait142 <chilinski.a@gmail.com>
2020-08-04 14:35:51 -0400

    Add playlist and other styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,11 @@ export const Header = () => {
     return(
         <div className="header">
             <div className="name">
-            <h1>Alexis Chilinski</h1>
-            <h3>Software Engineer</h3>
+                <h1>Alexis Chilinski</h1>
+                <h3>Software Engineer</h3>
+            </div>
+            <div>
+                <iframe src="https://open.spotify.com/embed/playlist/5cQqTdDQxLMUrI8CklVwwt" width="400" height="100" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             </div>
             {/* <Navbar/> */}
 
